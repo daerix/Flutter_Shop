ef9692721dd048c8197a680c1e48b8b79d6b0650
Virgile Sassano <virgilesassano@gmail.com>
2021-04-16 17:07:51 +0200

    Ajout du composant commentaries

--- flutter_shop/lib/main.dart
+++ flutter_shop/lib/main.dart
@@ -3,14 +3,15 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter_shop/models/commentary.dart';
 import 'package:flutter_shop/models/user.dart';
-import 'package:flutter_shop/widgets/commentary_page/commentary.dart';
+import 'package:flutter_shop/widgets/commentary_page/commentary_page.dart';
 
 List<Commentary> commentaries;
-
+User currentUser;
 void main() {
   commentaries = [];
+  currentUser = new User("Virgile Sassano");
   Random random = Random();
-  for (var i = 0; i < 5; i++) {
+  for (var i = 0; i < 2; i++) {
     Commentary comment = new Commentary();
     comment.comment = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vitae tellus commodo, tincidunt urna sed, ultrices nunc. Aliquam sed nibh felis. Mauris odio turpis, aliquet eu sem ut, iaculis interdum diam. Donec interdum viverra nisi, eu finibus leo cursus sit amet. Ut nec ipsum efficitur, bibendum nisl nec, ultrices neque. Curabitur consequat ullamcorper ligula. Aliquam eu metus libero. Duis rutrum a turpis at molestie. Sed tempor ipsum id viverra lobortis. Sed eu mollis mauris. Aliquam lectus purus, volutpat id ligula eget, tincidunt laoreet erat. Cras malesuada accumsan pharetra. Praesent nibh eros, faucibus vitae enim id, ullamcorper luctus ex.";
     comment.user = new User("User " + i.toString());
@@ -29,10 +30,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: SingleChildScrollView(
-          child: Column(children: [
-        for (var comment in commentaries) CommentaryWidget(commentary: comment)
-      ])),
+      home: CommentaryPageWidget(commentaries: commentaries, currentUser: currentUser),
     );
   }
 }
