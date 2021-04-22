9bbee5b9d856ff1022019f7c51f32ae6f53c2bd9
BFaycal <47598832+BFaycal@users.noreply.github.com>
2021-04-22 09:37:37 +0200

    add login

--- flutter_shop/lib/main.dart
+++ flutter_shop/lib/main.dart
@@ -3,7 +3,7 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter_shop/models/commentary.dart';
 import 'package:flutter_shop/models/user.dart';
-import 'package:flutter_shop/widgets/commentary_page/commentary_page.dart';
+import 'package:flutter_shop/widgets/Guest.dart';
 
 List<Commentary> commentaries;
 User currentUser;
@@ -13,7 +13,8 @@ void main() {
   Random random = Random();
   for (var i = 0; i < 2; i++) {
     Commentary comment = new Commentary();
-    comment.comment = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vitae tellus commodo, tincidunt urna sed, ultrices nunc. Aliquam sed nibh felis. Mauris odio turpis, aliquet eu sem ut, iaculis interdum diam. Donec interdum viverra nisi, eu finibus leo cursus sit amet. Ut nec ipsum efficitur, bibendum nisl nec, ultrices neque. Curabitur consequat ullamcorper ligula. Aliquam eu metus libero. Duis rutrum a turpis at molestie. Sed tempor ipsum id viverra lobortis. Sed eu mollis mauris. Aliquam lectus purus, volutpat id ligula eget, tincidunt laoreet erat. Cras malesuada accumsan pharetra. Praesent nibh eros, faucibus vitae enim id, ullamcorper luctus ex.";
+    comment.comment =
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vitae tellus commodo, tincidunt urna sed, ultrices nunc. Aliquam sed nibh felis. Mauris odio turpis, aliquet eu sem ut, iaculis interdum diam. Donec interdum viverra nisi, eu finibus leo cursus sit amet. Ut nec ipsum efficitur, bibendum nisl nec, ultrices neque. Curabitur consequat ullamcorper ligula. Aliquam eu metus libero. Duis rutrum a turpis at molestie. Sed tempor ipsum id viverra lobortis. Sed eu mollis mauris. Aliquam lectus purus, volutpat id ligula eget, tincidunt laoreet erat. Cras malesuada accumsan pharetra. Praesent nibh eros, faucibus vitae enim id, ullamcorper luctus ex.";
     comment.user = new User("User " + i.toString());
     comment.date = DateTime.now();
     comment.stars = random.nextInt(5);
@@ -26,11 +27,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Shop',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: CommentaryPageWidget(commentaries: commentaries, currentUser: currentUser),
+      home: GuestScreen(),
     );
   }
 }
